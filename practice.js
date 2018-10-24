0b856a92575ae2ceb715fadc0aeb2d4cbfbb40ff
Alex McDaniel <amcdaniel186@gmail.com>
2018-10-24 16:23:36 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+var favoritePhrase = "Don't be evil";
+
+console.log(favoritePhrase.indexOf("Don't"));
+
+console.log(favoritePhrase.indexOf("e"));
+// This is 7 because indexOf prints the FIRST occurance
+
+console.log(favoritePhrase.indexOf("z"));
+// It's not found so the index is -1
